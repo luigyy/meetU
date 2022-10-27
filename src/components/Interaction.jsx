d9c42c9b4260902ebb3b86ab854204bc3218adfd
luigyy <luigy.valverde@ucr.ac.cr>
2022-10-26 22:22:20 -0600

    added tooltip to interaction bar

--- src/components/Interaction.jsx
+++ src/components/Interaction.jsx
@@ -3,17 +3,26 @@ import { FcLike } from "react-icons/fc";
 import { FcDislike } from "react-icons/fc";
 import { BsArrowReturnLeft, BsArrowReturnRight } from "react-icons/bs";
 import { ImProfile } from "react-icons/im";
+import ReactTooltip from "react-tooltip";
 
 //======================
-const InteractionButton = ({ title, style, icon, bgOnHover }) => {
+const InteractionButton = ({ title, style, icon, bgOnHover, tooltip }) => {
   return (
-    <button
-      className={`rounded-3xl p-5 text-4xl ${
-        bgOnHover ? "hover:bg-red-200" : "hover:bg-gray-200"
-      }`}
-    >
-      {icon || title}
-    </button>
+    <>
+      <button
+        data-tip
+        data-for={title}
+        className={`rounded-3xl p-5 text-4xl ${
+          bgOnHover ? "hover:bg-red-200" : "hover:bg-gray-200"
+        }`}
+      >
+        {icon}
+      </button>
+      {/** test */}
+      <ReactTooltip id={title}>
+        <span>{title}</span>
+      </ReactTooltip>
+    </>
   );
 };
 
@@ -22,7 +31,9 @@ const Interaction = () => {
     <div className="flex justify-evenly  w-full fixed bottom-14 ">
       <InteractionButton title="Backward" icon={<BsArrowReturnLeft />} />
       <InteractionButton title="Like" icon={<FcLike />} bgOnHover="red" />
+      {/** */}
       <InteractionButton title="See Profile" icon={<ImProfile />} />
+      {/** */}
       <InteractionButton title="Pass" icon={<FcDislike />} />
       <InteractionButton title="Forward" icon={<BsArrowReturnRight />} />
     </div>
