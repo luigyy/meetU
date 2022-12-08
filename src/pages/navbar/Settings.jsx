5c64af06a1f9beb315bac84b100a6960686d3161
luigyy <luigy.valverde@ucr.ac.cr>
2022-12-08 17:21:30 -0600

    start working on editProfile

--- src/pages/navbar/Settings.jsx
+++ src/pages/navbar/Settings.jsx
@@ -3,13 +3,18 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import HeaderForNavbarButtons from "../../components/HeaderForNavbarButtons";
 import { useStateContext } from "../../contexts/ContextProvider";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
 
-const SettingsButton = ({ title, customFunc }) => {
+const SettingsButton = ({ title, customFunc, path }) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div
         className="p-5 text-gray-600 cursor-pointer flex justify-between hover:bg-gray-50"
-        onClick={customFunc}
+        onClick={() => {
+          if (customFunc) customFunc();
+          return path ? navigate(path) : null;
+        }}
       >
         <button>{title}</button>
         <AiOutlineArrowRight />
@@ -29,7 +34,7 @@ const Settings = () => {
   return (
     <div>
       <HeaderForNavbarButtons title="Settings" />
-      <SettingsButton title="Edit Profile" />
+      <SettingsButton title="Edit Profile" path="/editProfile" />
       <SettingsButton title="Log out" customFunc={handleLogout} />
     </div>
   );
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import Settings from "./pages/navbar/Settings";
 import Chats from "./pages/navbar/Chats";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
 
 export const protectedRoutes = [
   { path: "/", component: <Main /> },
   { path: "/settings", component: <Settings /> },
   { path: "/chats", component: <Chats /> },
   { path: "/Profile", component: <Profile /> },
+  { path: "/editProfile", component: <EditProfile /> },
 ];
 
 export const publicRoutes = [{ path: "/landing", component: <Landing /> }];
